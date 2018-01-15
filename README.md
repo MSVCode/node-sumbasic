31e0b2922029942b6807b916ff002ce4ed08ba32
Vincent <centralofmsv@gmail.com>
2018-01-15 12:13:04 +0800

    Change dependencies from natural to stemmer

--- README.md
+++ README.md
@@ -8,9 +8,10 @@ Here, the SumBasic are created as module inside `./src/index.js`.
 ### NPM package ###
 - Run `npm install node-sumbasic --save`
 - Require it in your project `const sumBasic = require('node-sumbasic')`
-- Call it `sumBasic(documents,50)`
+- Call it `sumBasic(documents, target_summary_length, minimum_sentence_length_considered [optional])`
+- Example `sumBasic(documents, 100)`
 
-### Demo run in project folder###
+### Demo run in project folder ###
 - Make sure to install the npm modules first `npm install`
 - In the project, run `node ./src/demo.js`
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-const natural = require('natural');
-const minSentenceLen = 3;
+const stemmer = require('stemmer');
 const titleFilter = ['a','about','above','after','again','against','all','am','an','and','any','are',"aren't",'as','at','be','because','been','before','being','below','between','both','but','by',"can't",'cannot','could',"couldn't",'did',"didn't",'do','does',"doesn't",'doing',"don't",'down','during','each','few','for','from','further','had',"hadn't",'has',"hasn't",'have',"haven't",'having','he',"he'd","he'll","he's",'her','here',"here's",'hers','herself','him','himself','his','how',"how's",'i',"i'd","i'll","i'm","i've",'if','in','into','is',"isn't",'it',"it's",'its','itself',"let's",'me','more','most',"mustn't",'my','myself','no','nor','not','of','off','on','once','only','or','other','ought','our','ours','ourselves','out','over','own','same',"shan't",'she',"she'd","she'll","she's",'should',"shouldn't",'so','some','such','than','that',"that's",'the','their','theirs','them','themselves','then','there',"there's",'these','they',"they'd","they'll","they're","they've",'this','those','through','to','too','under','until','up','very','was',"wasn't",'we',"we'd","we'll","we're","we've",'were',"weren't",'what',"what's",'when',"when's",'where',"where's",'which','while','who',"who's",'whom','why',"why's",'with',"won't",'would',"wouldn't",'you',"you'd","you'll","you're","you've",'your','yours','yourself','yourselves','zero'];
 
 function wpFrequency(bagOfWords, totalNumberOfWords){
@@ -21,7 +20,7 @@ function sortWordProb(bagOfWords, wordsProbability){
     return wordArray;
 }
 
-function swAveraged(sentenceObject, wordsProbability){
+function swAveraged(sentenceObject, wordsProbability, minSentenceLen){
     for (let i in sentenceObject){
         let wordLen = sentenceObject[i].contentWords.length;
         if (wordLen<minSentenceLen){
@@ -48,7 +47,7 @@ function documentToSentence_bagOfWords(documentList){
             const words = sentence.replace(/[^a-zA-Z0-9 ]/g,"").toLowerCase().split(" ");
             let contentWords = [];
             for (let j in words) {
-                const word = natural.PorterStemmer.stem(words[j]);//stemming
+                const word = stemmer(words[j]);//stemming
                 if (titleFilter.indexOf(word)==-1 && word!=""){//not a stopword
                     contentWords.push(word);
                     if (word in bagOfWords){
@@ -69,11 +68,12 @@ function documentToSentence_bagOfWords(documentList){
 }
 
 /**
- * Perform SumBasic operation
- * @param {string[]} documentList 
- * @param {number} targetWordCount 
+ * Perform SumBasic operation.
+ * @param {string[]} documentList List of the documents
+ * @param {number} targetWordCount Target summary length (minimum)
+ * @param {number} minSentenceLen Minimum length of a sentence to be weighted in the SumBasic process
  */
-function sumBasic(documentList, targetWordCount=10){
+function sumBasic(documentList, targetWordCount=10, minSentenceLen=3){
     const sentenceProcess = documentToSentence_bagOfWords(documentList);
     let sentenceObject = sentenceProcess.sentenceList;
     const bagOfWords = sentenceProcess.bagOfWords;//distinct
@@ -89,7 +89,7 @@ function sumBasic(documentList, targetWordCount=10){
         //move bag of words from object to array
         let wordArray = sortWordProb(bagOfWords, wordsProbability);
         //(re)compute sentence weight
-        sentenceObject = swAveraged(sentenceObject, wordsProbability);
+        sentenceObject = swAveraged(sentenceObject, wordsProbability, minSentenceLen);
         //sort the weights
         sentenceObject.sort(function(a,b){
             return b.sentenceWeight-a.sentenceWeight;
